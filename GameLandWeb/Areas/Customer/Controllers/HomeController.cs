4f3c80157314303b499ebf51425244659a04c747
Vladislav Korenev <korenevlad17@gmail.com>
2023-08-12 14:12:19 +0300

    Add-Home-Index (View of products)

--- GameLandWeb/Areas/Customer/Controllers/HomeController.cs
+++ GameLandWeb/Areas/Customer/Controllers/HomeController.cs
@@ -8,21 +8,24 @@ namespace GameLandWeb.Areas.Customer.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(IUnitOfWork unitOfWork)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
+            _logger = logger;
             _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category_of_product");
+            return View(productList);
         }
 
         
 
-
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
